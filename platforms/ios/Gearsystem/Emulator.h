0200938e3a5cbde2510e33b527cf3cfe35a1c74b
Ignacio Sanchez Gines <nacho@geardome.com>
2014-11-10 10:25:16 +0100

    added iOS 8 project

---
+++ platforms/ios/Gearsystem/Emulator.h
@@ -0,0 +1,62 @@
+/*
+ * Gearsystem - Sega Master System / Game Gear Emulator
+ * Copyright (C) 2013  Ignacio Sanchez
+ 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see http://www.gnu.org/licenses/
+ *
+ */
+
+#import <Foundation/Foundation.h>
+
+#ifdef __APPLE__
+#if TARGET_IPHONE_SIMULATOR == 1 || TARGET_OS_IPHONE == 1
+#define SDL_MAIN_HANDLED
+#endif
+#endif
+
+#import "../../../src/gearsystem.h"
+#import "EmulatorInput.h"
+
+@interface Emulator : NSObject
+{
+    GearsystemCore* theGearsystemCore;
+    GS_Color* theFrameBuffer;
+    GS_Color* theTexture;
+    EmulatorInput* theInput;
+    GLuint GBTexture;
+}
+
+@property (nonatomic) int multiplier;
+@property (nonatomic) BOOL retina;
+@property (nonatomic) BOOL iPad;
+
+- (void)update;
+- (void)draw;
+- (void)loadRomWithPath: (NSString *)filePath;
+- (void)keyPressed: (GS_Keys)key;
+- (void)keyReleased: (GS_Keys)key;
+- (void)pause;
+- (void)resume;
+- (BOOL)paused;
+- (void)reset;
+- (void)save;
+- (void)initGL;
+- (void)shutdownGL;
+- (void)renderFrame;
+- (void)setupTextureWithData: (GLvoid*) data;
+- (void)renderQuadWithViewportWidth: (int)viewportWidth andHeight: (int)viewportHeight andMirrorY: (BOOL)mirrorY;
+- (void)setAudio: (BOOL)enabled;
+- (void)resetAudio;
+
+@end
