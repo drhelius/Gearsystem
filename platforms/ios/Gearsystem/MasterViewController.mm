06c70ac4f972a5b90bd3987bbb94a3c7c8627e51
Ignacio Sanchez Gines <nacho@geardome.com>
2018-03-07 19:30:46 +0100

    fix rom extenions

--- platforms/ios/Gearsystem/MasterViewController.mm
+++ platforms/ios/Gearsystem/MasterViewController.mm
@@ -1,17 +1,17 @@
 /*
  * Gearsystem - Sega Master System / Game Gear Emulator
  * Copyright (C) 2013  Ignacio Sanchez
- 
+
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- 
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- 
+
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/
  *
@@ -34,7 +34,7 @@
         self.clearsSelectionOnViewWillAppear = NO;
         self.preferredContentSize = CGSizeMake(320.0, 600.0);
     }
-    
+
     self.theGLViewController = [[GLViewController alloc] initWithNibName:@"GLViewController" bundle:nil];
     self.theGLViewController.preferredFramesPerSecond = 60;
     self.theGLViewController.resumeOnDidBecomeActive = NO;
@@ -59,31 +59,31 @@
 - (void)reloadTableView
 {
     NSArray *paths = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES);
-    
+
     NSArray *files = nil;
-    
+
     if ([paths count] > 0)
     {
         NSFileManager *fileManager = [[NSFileManager alloc] init];
         NSArray *dirContents = [fileManager contentsOfDirectoryAtPath:[paths objectAtIndex:0] error:nil];
-        
+
         if ([dirContents count] > 0)
         {
-            NSArray *extensions = [NSArray arrayWithObjects:@"zip", @"gb", @"sgb", @"gbc", @"rom", @"dmg", @"cgb", @"ZIP", @"GB", @"SGB", @"GBC", @"ROM", @"DMG", @"CGB", nil];
+            NSArray *extensions = [NSArray arrayWithObjects:@"zip", @"sms", @"gg", @"rom", @"bin", @"ZIP", @"SMS", @"GG", @"ROM", @"BIN", nil];
             files = [dirContents filteredArrayUsingPredicate:[NSPredicate predicateWithFormat:@"pathExtension IN %@", extensions]];
         }
     }
-    
+
     self.sections = [[NSMutableDictionary alloc] init];
-    
+
     BOOL found;
-    
+
     for (NSString* rom in files)
     {
         NSString* c = [[rom substringToIndex:1] uppercaseString];
-        
+
         found = NO;
-        
+
         for (NSString* str in [self.sections allKeys])
         {
             if ([str isEqualToString:c])
@@ -91,27 +91,27 @@
                 found = YES;
             }
         }
-        
+
         if (!found)
         {
             [self.sections setValue:[[NSMutableArray alloc] init] forKey:c];
         }
     }
-    
+
     for (NSString* rom in files)
     {
         [[self.sections objectForKey:[[rom substringToIndex:1] uppercaseString]] addObject:rom];
     }
-    
+
     [self.tableView reloadData];
-    
+
 }
 
 - (void)loadWithROM:(NSString *)rom
 {
     _openedFromOtherApp = YES;
     _openedFromOtherAppRom = rom;
-    
+
     if (self.splitViewController.collapsed)
     {
         id vc = [[self.splitViewController.viewControllers lastObject] topViewController];
@@ -122,7 +122,7 @@
             return;
         }
     }
-    
+
     [self performSegueWithIdentifier: @"showDetail" sender: self];
 }
 
@@ -130,9 +130,9 @@
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
-        
+
         NSString* rom;
-        
+
         if (_openedFromOtherApp)
         {
             _openedFromOtherApp = NO;
@@ -143,7 +143,7 @@
             NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
             rom = [[self.sections valueForKey:[[[self.sections allKeys] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)] objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
         }
-        
+
         DetailViewController *controller = (DetailViewController *)[[segue destinationViewController] topViewController];
         [controller setDetailItem:rom];
         [controller setTheGLViewController:self.theGLViewController];
@@ -174,7 +174,7 @@
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
     static NSString *CellIdentifier = @"Cell";
-    
+
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
@@ -182,9 +182,9 @@
             cell.accessoryType = UITableViewCellAccessoryDisclosureIndicator;
         }
     }
-    
+
     NSString* rom = [[self.sections valueForKey:[[[self.sections allKeys] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)] objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
-    
+
     cell.textLabel.text = [rom stringByDeletingPathExtension];
     cell.textLabel.adjustsFontSizeToFitWidth = YES;
     cell.selectionStyle = UITableViewCellSelectionStyleGray;
@@ -199,19 +199,19 @@
     if (editingStyle == UITableViewCellEditingStyleDelete)
     {
         NSString* rom = [[self.sections valueForKey:[[[self.sections allKeys] sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)] objectAtIndex:indexPath.section]] objectAtIndex:indexPath.row];
-        
+
         NSString *documentsDirectory = [NSSearchPathForDirectoriesInDomains(NSDocumentDirectory, NSUserDomainMask, YES) objectAtIndex:0];
         NSError* error;
         NSFileManager *fileManager = [[NSFileManager alloc] init];
-        
+
         NSString* deletePath = [NSString stringWithFormat:@"%@/%@", documentsDirectory, rom];
-        
+
         if ([fileManager removeItemAtPath:deletePath error:&error])
         {
             [tableView beginUpdates];
-            
+
             [[self.sections objectForKey:[[rom substringToIndex:1] uppercaseString]] removeObject:rom];
-            
+
             if ([[self.sections objectForKey:[[rom substringToIndex:1] uppercaseString]] count] > 0)
             {
                 [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
@@ -219,18 +219,18 @@
             else
             {
                 [self.sections removeObjectForKey:[[rom substringToIndex:1] uppercaseString]];
-                
+
                 [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
                 [tableView deleteSections:[NSIndexSet indexSetWithIndex:indexPath.section]withRowAnimation:UITableViewRowAnimationFade];
             }
-            
+
             [tableView endUpdates];
         }
         else
         {
             NSLog(@"ERROR %@", [error localizedDescription]);
         }
-        
+
         [tableView reloadData];
     }
 }
