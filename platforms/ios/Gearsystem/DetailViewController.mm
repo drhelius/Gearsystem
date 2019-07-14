626542624fdfe5d9e7646d90b8d4943c8f4171bc
Ignacio Sanchez Gines <nacho@geardome.com>
2019-07-14 19:49:56 +0200

    tell iPhone X+ to hide the home indicator

--- platforms/ios/Gearsystem/DetailViewController.mm
+++ platforms/ios/Gearsystem/DetailViewController.mm
@@ -1,17 +1,17 @@
 /*
  * Gearsystem - Sega Master System / Game Gear Emulator
  * Copyright (C) 2013  Ignacio Sanchez
- 
+
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * any later version.
- 
+
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
- 
+
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see http://www.gnu.org/licenses/
  *
@@ -30,20 +30,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+
     self.view.multipleTouchEnabled = YES;
-    
+
     UIBarButtonItem *save = [[UIBarButtonItem alloc] initWithTitle:@"Save" style:UIBarButtonItemStylePlain target:self action:@selector(saveState)];
     UIBarButtonItem *load = [[UIBarButtonItem alloc] initWithTitle:@"Load" style:UIBarButtonItemStylePlain target:self action:@selector(loadState)];
-    
+
     self.navigationItem.rightBarButtonItems = @[load, save];
-    
+
     CGRect screenBounds = [[UIScreen mainScreen] bounds];
-    
+
     int scrW = 320;
     int scrH = 416;
     NSString* imageName = @"sms_832.jpg";
-    
+
     switch ((int)screenBounds.size.height)
     {
         case 480:
@@ -95,24 +95,30 @@
             break;
         }
     }
-    
+
     UIImageView *imgView = [[UIImageView alloc] init];
     imgView.image = [UIImage imageNamed:imageName];
     imgView.frame = CGRectMake(0, 0, scrW, scrH);
-    
+
     [self.view addSubview:imgView];
     [self.view addSubview:self.theGLViewController.view];
-    
+
     if (self.detailItem)
     {
         [self.theGLViewController loadRomWithName:self.detailItem];
     }
 }
 
+- (BOOL)prefersHomeIndicatorAutoHidden
+{
+    return YES;
+}
+
+
 - (BOOL)shouldAutorotate
 {
     UIInterfaceOrientation orientation = [[UIApplication sharedApplication] statusBarOrientation];
-    
+
     return (orientation == UIInterfaceOrientationPortrait) || (orientation == UIInterfaceOrientationPortraitUpsideDown);;
 }
 
