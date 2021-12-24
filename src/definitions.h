e6b5e357be11aedbd1e39e30095333d0b923f6c8
Ignacio Sanchez Gines <nacho@geardome.com>
2021-12-24 15:22:35 +0100

    Push version to 3.4.2

--- src/definitions.h
+++ src/definitions.h
@@ -38,7 +38,7 @@
 #endif
 
 #define GEARSYSTEM_TITLE "Gearsystem"
-#define GEARSYSTEM_VERSION "3.4.1"
+#define GEARSYSTEM_VERSION "3.4.2"
 
 #ifndef EMULATOR_BUILD
 #define EMULATOR_BUILD "undefined"
