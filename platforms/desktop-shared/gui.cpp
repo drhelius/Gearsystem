724e0e708df5946715441f19ed8235aa32343f55
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2024-12-29 17:33:40 +0100

    Fix url links in about window

--- platforms/desktop-shared/gui.cpp
+++ platforms/desktop-shared/gui.cpp
@@ -1599,9 +1599,10 @@ static void popup_modal_about(void)
 
         ImGui::TextColored(orange, "  By Ignacio Sánchez (DrHelius)");
         ImGui::Text(" "); ImGui::SameLine();
-        ImGui::TextLink("https://github.com/drhelius/Gearsystem");
+        //ImGui::TextLink("https://github.com/drhelius/Gearsystem");
+        ImGui::TextLinkOpenURL("https://github.com/drhelius/Gearsystem", "https://github.com/drhelius/Gearsystem");
         ImGui::Text(" "); ImGui::SameLine();
-        ImGui::TextLink("https://x.com/drhelius");
+        ImGui::TextLinkOpenURL("https://x.com/drhelius");
         ImGui::NewLine();
 
         ImGui::PopFont();
