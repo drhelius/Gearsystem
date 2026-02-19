454d4eb830dbc07eb66e438222f27a208f7f83b4
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-02-19 20:25:09 +0100

    Improve audio latency

--- platforms/shared/desktop/gui_menus.cpp
+++ platforms/shared/desktop/gui_menus.cpp
@@ -1110,7 +1110,8 @@ static void menu_audio(void)
                 ImGui::BeginTooltip();
                 ImGui::Text("Lower values reduce audio latency.");
                 ImGui::Text("Higher values prevent audio underruns.");
-                ImGui::Text("Audio latency: %.0f ms", latency_ms);
+                ImGui::Text("Enabling VSync may force higher buffer counts.");
+                ImGui::Text("Current audio latency: %.0f ms", latency_ms);
                 ImGui::EndTooltip();
             }
             ImGui::EndMenu();
