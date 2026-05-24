7b4d9aa8640ec4392a1e9bc3bd1cc9f5687c328b
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-05-24 22:04:25 +0200

    Fail save states on undersized output buffers

--- src/GearsystemCore.cpp
+++ src/GearsystemCore.cpp
@@ -760,6 +760,12 @@ bool GearsystemCore::SaveState(u8* buffer, size_t& size, bool screenshot)
             return false;
         }
 
+        if (!direct_stream.good())
+        {
+            Error("Failed to save state to buffer: output buffer is too small");
+            return false;
+        }
+
         size = direct_stream.size();
         return true;
     }
