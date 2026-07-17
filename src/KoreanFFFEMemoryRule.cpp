96995b3ea78bacecfd9428fa5ad27887def49bad
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-07-18 00:48:42 +0200

    Preserve Korean FFFE mapper RAM mirroring

--- src/KoreanFFFEMemoryRule.cpp
+++ src/KoreanFFFEMemoryRule.cpp
@@ -89,8 +89,6 @@ void KoreanFFFEMemoryRule::PerformWrite(u16 address, u8 value)
         m_iPageAddress[4] = 0x2000 * m_iPage[4];
         m_iPageAddress[5] = 0x2000 * m_iPage[5];
         TraceBankSwitch(address, value);
-
-        return;
     }
 
     if (address >= 0xC000)
