75233dfd86a1e77afb6056bbbccd7b351f4893b8
Ignacio Sanchez Gines <863613+drhelius@users.noreply.github.com>
2026-07-27 15:16:25 +0200

    Preserve carry when updating immediate input addresses

--- src/opcodes.cpp
+++ src/opcodes.cpp
@@ -1442,7 +1442,7 @@ void Processor::ExecuteInputLastCycle()
     u8 port = m_pMemory->Read(PC.GetValue());
     PC.Increment();
     AF.SetHigh(m_pIOPorts->DoInput(port));
-    WZ.SetValue((a << 8) | (port + 1));
+    WZ.SetValue((a << 8) + port + 1);
     m_iTStates += 1;
     m_bInputLastCycle = false;
 }
